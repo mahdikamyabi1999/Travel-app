f29e891b691cefd5748b15987400ff42ecca2a5d
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-02-29 15:49:48 +0330

    make Camp section and make it responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative">
+        <main className="relative overflow-hidden">
         {children}
         </main>
         <Footer />
