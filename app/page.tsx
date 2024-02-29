15a1de16103131e196456386ee47a0ea7f85997e
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-02-29 17:06:27 +0330

    complete get app section and make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ type buttonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 }
-const Button = ({type,title,icon,variant}: buttonProps) => {
+const Button = ({type,title,icon,variant,full}: buttonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>{icon && <Image src={icon} alt={title} width={24} height={24}/>}
-    <label className="bold-16 whitespace-nowrap">{title}</label></button>
+    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} type={type}>{icon && <Image src={icon} alt={title} width={24} height={24}/>}
+    <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label></button>
   )
 }
 
